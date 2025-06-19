17ad2722be612763367979e7c23c048d2ec18dd0
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-06-19 08:15:46 +0530

    created Theme context and used in code

---
+++ app/context/ThemeContext.tsx
@@ -0,0 +1,61 @@
+"use client";
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface ThemeContextTypes {
+  mode: ThemeMode,
+  setMode: Dispatch<SetStateAction<ThemeMode>>
+}
+export type ThemeMode = 'light' | 'dark' | 'system'
+
+const ThemeContext = createContext<ThemeContextTypes | null>(null);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [mode, setMode] = useState<ThemeMode>('system');
+
+  const applyTheme = (newMode:ThemeMode) =>{
+    if(newMode === 'light'){
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('themeMode','light')
+    } else if(newMode === 'dark'){
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('themeMode','dark')
+    } else {
+        const preferDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+        document.documentElement.classList.toggle('dark',preferDark)
+        localStorage.removeItem('themeMode')
+    }
+  }
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('themeMode')
+    const initialMode = storedTheme === 'dark' || storedTheme ==='light' ? storedTheme : 'system' 
+    setMode(initialMode)
+    applyTheme(initialMode)
+    
+    const mediaQuery = window.matchMedia('(prefers-color-scheme : dark)')
+    const handleChange = (e:MediaQueryListEvent) =>{
+        document.documentElement.classList.toggle('dark',e.matches)
+    }
+
+    mediaQuery.addEventListener('change',handleChange)
+    return ()=> mediaQuery.removeEventListener('change',handleChange)
+}, []);
+
+useEffect(()=>{
+    applyTheme(mode)
+},[mode])
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => useContext(ThemeContext);
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+'use client'
+import { ThemeMode, useDarkMode } from "./context/ThemeContext";
+
 export default function Home() {
+  const {mode,setMode} = useDarkMode()!
+  const handleThemeChange = (e:React.ChangeEvent<HTMLSelectElement>)=>{
+    setMode(e.target.value as ThemeMode) 
+  }
   return (
-    <div className="bg-red-400">
+    <div className="bg-white dark:bg-gray-500 flex flex-col items-center justify-center min-h-screen transition-colors">
       Hii
+      {mode}
+      <select onChange={(e)=>handleThemeChange(e)}>
+        <option value={'light'}>light</option>
+        <option value={'dark'}>dark</option>
+        <option value={'system'}>system</option>
+      </select>
     </div>
   );
 }
